165b03c296af4cd6be96f775160c2d4f39fb4a26
Emre <emrekemalaksel@gmail.com>
2025-08-07 14:33:43 +0300

    Possible fix to REDIS_URL

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,13 +1,23 @@
 import Redis from 'ioredis';
 import dotenv from 'dotenv';
+import { URL } from 'url';
 
 dotenv.config();
 
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+const db = parseInt(process.env.REDIS_DB || '0', 10);
+
+const parsed = new URL(redisUrl);
+
 export const redisConfig = {
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
-  db: parseInt(process.env.REDIS_DB || '0', 10),
+  host: parsed.hostname,
+  port: Number(parsed.port || 6379),
+  password: parsed.password || undefined,
+  db,
 };
 
-const redis = new Redis(redisConfig);
+const redis = new Redis({
+  ...redisConfig,
+});
 
 export default redis;
